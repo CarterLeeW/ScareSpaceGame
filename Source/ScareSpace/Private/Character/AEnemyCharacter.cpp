f4c4de19a145e53bae82c129f822719b4662596e
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-10-20 22:34:05 -0400

    created first main two character classes

---
+++ Source/ScareSpace/Private/Character/AEnemyCharacter.cpp
@@ -0,0 +1,34 @@
+// Copyright Carter Wooton
+
+
+#include "Character/AEnemyCharacter.h"
+
+// Sets default values
+AAEnemyCharacter::AAEnemyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAEnemyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AAEnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
