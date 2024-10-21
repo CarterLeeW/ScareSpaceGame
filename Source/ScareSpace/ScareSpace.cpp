f2ebfeebbed20e8680e7a4730eaa69126eae9260
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-10-20 21:15:51 -0400

    first commit

---
+++ Source/ScareSpace/ScareSpace.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ScareSpace.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ScareSpace, "ScareSpace" );
+ 
---
+++ Source/ScareSpace/ScareSpaceGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ScareSpaceGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AScareSpaceGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AScareSpaceGameMode();
+};
+
+
+
