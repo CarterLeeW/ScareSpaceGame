0efc645f3fa59edf1f7a81d36631da82cc573a00
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2025-04-05 16:48:21 -0400

    GetInteractableComponent is now available in BP

--- Source/ScareSpace/Public/Interaction/Interactable.h
+++ Source/ScareSpace/Public/Interaction/Interactable.h
@@ -6,6 +6,8 @@
 #include "UObject/Interface.h"
 #include "Interactable.generated.h"
 
+class UInteractableComponent;
+
 // This class does not need to be modified.
 UINTERFACE(MinimalAPI)
 class UInteractable : public UInterface
@@ -23,8 +25,9 @@ class SCARESPACE_API IInteractable
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	/*
-	* Should return the Interaction Component associated with the object to be interacted with.
+	* Should return the Interactable Component associated with the object to be interacted with.
 	* This is required for the player to interact with the object.
 	*/
-	virtual void GetInteractionComponent() = 0;
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	UInteractableComponent* GetInteractableComponent();
 };
