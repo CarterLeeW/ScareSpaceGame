72252c2e2d0bfcfd2c026619ef3dc5579fd4d576
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2025-12-30 16:03:55 -0500

    stuck in physics rotation hell
    have to try using the rotation method.

--- Source/ScareSpace/Private/Interaction/PivotableComponent.cpp
+++ Source/ScareSpace/Private/Interaction/PivotableComponent.cpp
@@ -2,13 +2,12 @@
 
 
 #include "Interaction/PivotableComponent.h"
-#include "Components/PrimitiveComponent.h"
-#include "PhysicsEngine/PhysicsConstraintComponent.h"
+#include "Logging/ScareSpaceLogs.h"
 
 UPivotableComponent::UPivotableComponent()
 {
     PrimaryComponentTick.bCanEverTick = true;
-    PrimaryComponentTick.bStartWithTickEnabled = false;
+    PrimaryComponentTick.bStartWithTickEnabled = true;
 	InteractableType = EInteractableType::Pivotable;
 }
 
@@ -26,95 +25,10 @@ void UPivotableComponent::TickComponent(float DeltaTime, ELevelTick TickType, FA
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	
-    if (!PivotableMesh || !ConstraintComponent) return;
-
-    if (bEnableLatching && !bIsLinearMovement)
-    {
-        // Get current angle (Assuming Yaw/Z rotation relative to parent)
-        float CurrentYaw = PivotableMesh->GetRelativeRotation().Yaw;
-
-        bool bNearMin = FMath::IsNearlyEqual(CurrentYaw, MinLimit, LatchAngleThreshold);
-        bool bNearMax = FMath::IsNearlyEqual(CurrentYaw, MaxLimit, LatchAngleThreshold);
-
-        if (bNearMin)
-        {
-            // Snap to Closed (Min)
-            ConstraintComponent->SetAngularOrientationTarget(FRotator(0, MinLimit, 0));
-            ConstraintComponent->SetAngularOrientationDrive(true, true);
-            ConstraintComponent->SetAngularDriveParams(LatchStrength, 100.0f, 0.0f);
-        }
-        else if (bNearMax)
-        {
-            // Snap to Open (Max)
-            ConstraintComponent->SetAngularOrientationTarget(FRotator(0, MaxLimit, 0));
-            ConstraintComponent->SetAngularOrientationDrive(true, true);
-            ConstraintComponent->SetAngularDriveParams(LatchStrength, 100.0f, 0.0f);
-        }
-        else
-        {
-            // Free Swing (Disable Motor)
-            ConstraintComponent->SetAngularOrientationDrive(false, false);
-        }
-
-        // stay awake
-        if (bIsBeingHeld) return;
-
-        // Condition B: If the door has stopped moving, go back to sleep.
-        if (PivotableMesh->GetPhysicsAngularVelocityInDegrees().IsNearlyZero(1.0f))
-        {
-            SetComponentTickEnabled(false);
-        }
-    }
 }
 
 void UPivotableComponent::BeginPlay()
 {
 	Super::BeginPlay();
-    if (!PivotableParentMeshName.IsNone())
-    {
-        TArray<UPrimitiveComponent*> AllComps;
-        GetOwner()->GetComponents(AllComps);
-
-        for (UPrimitiveComponent* Comp : AllComps)
-        {
-            // check if the component's name matches our variable
-            if (Comp->GetFName() == PivotableParentMeshName)
-            {
-                PivotableMesh = Comp;
-                break; // Found it, stop searching
-            }
-        }
-    }
-
-    if (PivotableMesh)
-    {
-        // Ensure it generates wake events (required for the optimization to work)
-        PivotableMesh->BodyInstance.bGenerateWakeEvents = true;
-
-        // Bind the event
-        PivotableMesh->OnComponentWake.AddDynamic(this, &UPivotableComponent::OnMeshWake);
-    }
-    else
-    {
-        UE_LOG(LogTemp, Error, TEXT("%s: Could not find a mesh named '%s'! Check your spelling."),
-            *GetOwner()->GetName(),
-            *PivotableParentMeshName.ToString());
-
-        // Disable everything to prevent crashes
-        SetComponentTickEnabled(false);
-        return;
-    }
-
-    ConstraintComponent = GetOwner()->FindComponentByClass<UPhysicsConstraintComponent>();
-    if (!ConstraintComponent)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("%s: PivotableComponent needs a PhysicsConstraintComponent to work!"), *GetOwner()->GetName());
-    }
-    // Start Asleep
-    SetComponentTickEnabled(false);
-}
-
-void UPivotableComponent::OnMeshWake(UPrimitiveComponent* WakingComponent, FName BoneName)
-{
-	SetComponentTickEnabled(true);
+    
 }
--- Source/ScareSpace/Public/Interaction/PivotableComponent.h
+++ Source/ScareSpace/Public/Interaction/PivotableComponent.h
@@ -6,8 +6,6 @@
 #include "Interaction/InteractableComponent.h"
 #include "PivotableComponent.generated.h"
 
-class UPhysicsConstraintComponent;
-
 /**
  * 
  */
@@ -31,42 +29,9 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Pivotable")
 	FName PivotableParentMeshName;
 
-	// Does this object snap shut/open?
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Latching")
-	bool bEnableLatching = true;
-
-	// Angle tolerance (degrees) to trigger the latch (e.g., 0-10 deg)
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Latching")
-	float LatchAngleThreshold = 10.0f;
-
-	// How strong the "Magnet" pulls
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Latching")
-	float LatchStrength = 2000.0f;
-
-	// Movement Limits (Used by Interactor for Clamping)
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Limits")
-	bool bIsLinearMovement = false; // False = Rotating Door, True = Sliding Drawer
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Limits")
-	float MinLimit = 0.0f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Limits")
-	float MaxLimit = 90.0f;
-
 protected:
 	virtual void BeginPlay() override;
 
-private:
-	UFUNCTION()
-	void OnMeshWake(UPrimitiveComponent* WakingComponent, FName BoneName);
-
-	// Gets set so we don't have to keep looking for it
-	UPROPERTY()
-	TObjectPtr<UPrimitiveComponent> PivotableMesh = nullptr;
-
-	// For dealing with the angular motor
-	UPROPERTY()
-	TObjectPtr<UPhysicsConstraintComponent> ConstraintComponent;
-
+	UPROPERTY(BlueprintReadOnly, Category = "Pivotable")
 	bool bIsBeingHeld = false;
 };
