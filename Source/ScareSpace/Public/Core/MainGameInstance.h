a16f16af59485c997956bd6aeb3a428d23ac4795
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2026-05-06 20:41:18 -0400

    Floor expanded

---
+++ Source/ScareSpace/Public/Core/MainGameInstance.h
@@ -0,0 +1,17 @@
+// Copyright Carter Wooton
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MainGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCARESPACE_API UMainGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
